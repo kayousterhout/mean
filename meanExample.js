1ba66d93d81b7c657c0991c9a92e801ad815d536
Kay Ousterhout <kay@quilt.io>
2017-09-29 13:21:32 -0700

    blueprint: Mockup of potential new createDeployment

--- meanExample.js
+++ meanExample.js
@@ -5,7 +5,6 @@ const utils = require('./utils.js');
 // Replication to use for the node application
 // and Mongo.
 const count = 2;
-const infrastructure = quilt.createDeployment();
 
 const machine = new quilt.Machine({
   provider: 'Amazon',
@@ -15,8 +14,22 @@ const machine = new quilt.Machine({
 
 utils.addSshKey(machine);
 
-infrastructure.deploy(machine.asMaster());
-infrastructure.deploy(machine.asWorker().replicate(count));
+// A few questions:
+// (1) Can you add workers later?  I can imagine this potentially
+//     being something we'd want to add.
+// (2) Another idea I had was to take a worker machine and a count,
+//     since we're not sophisticated about scheduling, so users may
+//     find it misleading if they create a few types of worker machines.
+//     But then I realized that makes placement not work.
+// (3) Floating IPs seem a bit ugly here, since the user still has to
+//     pull out one of these workers and assign it a floating IP
+//     (I guess this applies to placement constraints in general?).
+//     This is a little uglier here because all of the workers are
+//     added when the deployment is created, so a blueprint couldn't,
+//     for example, take in the deployment, and add one worker with a
+//     floating IP that's specific to the deployment.  I wonder if this
+//     is a deeper problem with the current floating IP API though.
+const infrastructure = quilt.createDeployment(machine, machine.replicate(count));
 
 const nodeRepository = 'https://github.com/quilt/node-todo.git';
 const mean = new Mean(count, nodeRepository);
